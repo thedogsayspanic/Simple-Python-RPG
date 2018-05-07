7530b993af0f6be7720d087bff48b0d7f9ae694a
thedogsayspanic <nolenty@gmail.com>
2018-05-06 22:21:17 -0500

    Add hostile and friendly classes that are able to interact

--- bin/Entity.py
+++ bin/Entity.py
@@ -2,14 +2,35 @@ from NameGenerator import GenName
 import random
 
 class Entity:
-	def __init__(self, name=None, health=100, attack=10, attackAdvantage=False, defense=10, defenseAdvantage=False):
+	def __init__(self, name=None):
 		self.name = name if name is not None else GenName().New()
-		self.health = health
-		self.attack = attack
-		self.attackAdvantage = attackAdvantage
-		self.defense = defense
-		self.defenseAdvantage = defenseAdvantage
+		self.health = 1
+		self.attack = .05
+	def onMessage(self, message):
+		if message.msgType == "damage":
+			print("{0} deals {1} damage to {2}").format(message.msgFrom,message.msgData,message.msgTo)
+			self.health -= message.msgFrom.attack
+		else:
+			print("I dunno what that means...")
 
+class Hostile(Entity):
+	def __init__(self):
+		self.abilities = []
+		super().__init__()
+
+class Friendly(Entity):
+	def __init__(self, recruitable=False, recruited=False):
+		self.recruitable = recruitable
+		self.recruited = recruited
+		self.abilities = []
+		super().__init__()
+	def onMessage(self, message):
+		if message.msgType == "recruit":
+			if self.recruitable == True:
+				self.recruited = True
+
+		
+'''
 	def attack_creature(self, Entity):
 		damage = int(random.randint(round(self.attack*.5), round(self.attack*1.5)))
 		if Entity.defenseAdvantage is True and (Entity.defense * 2) - damage < 0:
@@ -24,3 +45,4 @@ class Entity:
 	def defend(self):
 		self.defenseAdvantage = True
 		print("%s enters a defensive stance.\n" % (self.name))
+'''
