0bd117ebc670b35ecc21473ef85749ec77dc2ea5
thedogsayspanic <nolenty@gmail.com>
2018-05-29 14:45:19 -0500

    Add showAll output

--- bin/MessageQueue.py
+++ bin/MessageQueue.py
@@ -7,7 +7,9 @@ class Message:
         self.msgTo = msgTo
         self.msgFrom = msgFrom
         self.msgType = msgType
-        self.msgData = msgData
+        self.msgData = msgData 
+    def __str__(self):
+        return "msgTo: %s msgFrom: %s msgType: %s msgData: %s" % (self.msgTo, self.msgFrom, self.msgType, self.msgData)
 
 class MessageQueue:
     def __init__(self):
@@ -15,7 +17,7 @@ class MessageQueue:
     def add(self, message):
         self.messages.append(message)
     def showAll(self):
-        print(self.messages)
+        print(self.messages.items())
     def dispatch(self):
         for i in range(len(self.messages)):
             msg = self.messages[i]
